f3eb441e24368910937b3a7e146206de7b5105b2
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2023-05-27 17:35:34 +0200

    calculate the latency
    Print the latency in the file given in parameter.

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,17 @@ fn main() {
             .value_name("INTERFACE")
             .help("Sets the network interface to listen on")
             .takes_value(true))
+        .arg(Arg::with_name("output")
+            .short('o')
+            .long("output")
+            .value_name("FILE")
+            .help("Sets the output file to write the latency")
+            .takes_value(true))
         .get_matches();
 
     // Gets a value for interface if supplied by user
     let interface = matches.value_of("interface").unwrap_or("default");
-    sv_pcap_subscriber::subscribe(interface);
+    // Gets a value for output file if supplied by user
+    let output_file = matches.value_of("output").unwrap_or("latency.txt");
+    sv_pcap_subscriber::subscribe(interface, output_file);
 }
--- src/sv_pcap_subscriber.rs
+++ src/sv_pcap_subscriber.rs
@@ -1,17 +1,40 @@
 extern crate pcap;
+use std::time::Instant;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
 
-pub fn subscribe(interface: &str) {
+pub fn subscribe(interface: &str, output_file: &str) {
     let mut cap = pcap::Capture::from_device(interface) // use the provided interface
-        .unwrap()
+        .expect("Error opening device")
         .promisc(true)
         .snaplen(5000)  // Configure the capture length
-        .tstamp_type(pcap::TimestampType::Adapter)
+        .tstamp_type(pcap::TimestampType::AdapterUnsynced) // Enable hardware timestamping
         .open()
-        .unwrap();
+        .expect("Error opening pcap");
 
-    cap.filter("ether proto 0x88ba", true).unwrap(); // Add a filter for IEC 61850 Sampled Values
+    cap.filter("ether proto 0x88ba", true).expect("Error setting filter"); // Add a filter for IEC 61850 Sampled Values
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(output_file)
+        .expect("Error opening file");
 
     while let Ok(packet) = cap.next_packet() {
-        println!("received packet! {:?}", packet);
+        // Get the current timestamp
+        let timestamp =  Instant::now();
+        // Get the packet timestamp
+        let packet_timestamp = packet.header.ts.tv_sec as u64 * 1000000 + packet.header.ts.tv_usec as u64;
+        // Calculate the latency
+        let latency = timestamp.elapsed().as_micros() as u64 - packet_timestamp;
+        // write latency to file
+        if let Err(e) = writeln!(file, "latency {} {}", timestamp.elapsed().as_micros(), latency) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+        // Get the packet length
+        // let packet_length = packet.header.len;
+        // Parse IEC 61850 Sampled Values
+        //let sv = sv_parser(&packet.data[42..packet_length as usize]);
     }
 }
